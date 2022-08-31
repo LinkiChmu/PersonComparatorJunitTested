6c2dc66e3d9bafe166dae9bdc92492da1f8cec45
chmutenkoa <alenachm@gmail.com>
2022-08-31 19:08:28 +0300

    Class PersonComparator to lambda

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
@@ -10,7 +11,21 @@ public class Main {
         people.add(new Person("Руслан", "Кара Мурза Хоттаб", 20));
         people.add(new Person("Иван", "Петров", 45));
         System.out.println(people);
-        Collections.sort(people, new PersonComparator(3));
+
+        Comparator<Person> myComparator = (o1, o2) -> {
+            int nobility1 = o1.getSurname().split("(?U)\\W+").length;
+            int nobility2 = o2.getSurname().split("(?U)\\W+").length;
+            int maxNumberWordsToCompare = 3;
+            // if both surnames consist of the number of words not less than the maximum, compare them by age
+            if (nobility1 >= maxNumberWordsToCompare && nobility2 >= maxNumberWordsToCompare) {
+                return o1.compareTo(o2);
+            } else {
+                int compareNobility = Integer.compare(nobility1, nobility2);
+                // if they equal, compare ages of persons
+                return (compareNobility == 0) ? o1.compareTo(o2) : compareNobility;
+            }
+        };
+        Collections.sort(people, myComparator);
         System.out.println(people);
     }
 }
